5a7cc5574e86509d548e7dc78f450f89eb6d68c4
Nicolae Casîr <nicolae.casir@gmail.com>
2019-10-14 15:58:12 +0300

    removed tkinter and added rec from file

--- 1.py
+++ 1.py
@@ -2,6 +2,11 @@ import wolframalpha
 import wikipedia
 from tkinter import *
 import speech_recognition as sr
+from os import path
+import re
+
+wikipedia.set_lang("ro")
+AUDIO_FILE = path.join(path.dirname(path.realpath(__file__)), "test.aif")
 
 while True:
 	r = sr.Recognizer()
@@ -11,35 +16,34 @@ while True:
 		audio = r.listen(source)
 		try:
 			print("Regognizing....")
-			text = r.recognize_google(audio)
+			text = r.recognize_google(audio, language="ro")
 			print('You said: ' + text)
 			if text == "stop":
 				print("Program will exit.")
 				break
+			elif text == 'citește din fișier':
+				with sr.AudioFile(AUDIO_FILE) as source:
+					print("Please wait... reading from file...")
+					audio = r.record(source)
+					text = r.recognize_google(audio, language="ro")
+					print('Text from your FILE:\n ' + text + "\n")
 			else:
 				window = Tk()
 				window.geometry("700x600")
 				try:
-					app_id = "enter your app id here"
+					app_id = "V67GTV-29YYGUQ2GX"
 					client = wolframalpha.Client(app_id)
 					res = client.query(text)
 					answer = next(res.results).text
 					print("Answer from Wolfram|Alpha:")
 					print(answer)
-					label1 = Label(window, justify=LEFT, wraplength=650, compound=CENTER, padx=10, text=answer, font='times 15 bold')
-					label1.pack()
-					window.after(5000, lambda: window.destroy())
-					mainloop()
 				except Exception as e:
 					print("No results from Wolfram|Alpha. Trying wikipedia...")
 					answer = wikipedia.summary(text)
 					print("Answer from Wikipedia:")
 					print(answer)
-					label1 = Label(window, justify=LEFT, wraplength=650, compound=CENTER, padx=10, text=answer, font='times 15 bold')
-					label1.pack()
-					window.after(500000, lambda: window.destroy())
-					mainloop()
 		except Exception as e:
 			print(e)
-			answer = "Sorry we cannt hear you"
+			print("Searching similar pages close to your request....")
+			answer = wikipedia.search(text)
 			print(answer)
